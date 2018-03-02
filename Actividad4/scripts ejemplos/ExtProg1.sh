2c2ff47ea6fcafc3f9dcfe3b6ebd794fd4aedaa9
Demidekeract <35710619+Demidekeract@users.noreply.github.com>
2018-03-02 23:52:40 +0000

    Add files via upload

---
+++ Actividad4/scripts ejemplos/ExtProg1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+    HTML_FILES=`find / -name "*.html" -print`
+    echo "$HTML_FILES" | grep "/index.html$"
+    echo "$HTML_FILES" | grep "/contents.html$"
---
+++ Actividad4/scripts ejemplos/Introduccion.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Esto es un comentario
+echo Hello World      #Esto tambien es un comentario
